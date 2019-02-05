311dd614e38b24ce8e8bc0c55bd1a7e82576001c
Joppert123 <44192052+Joppert123@users.noreply.github.com>
2019-02-05 12:48:51 +0100

    Data afgemaakt

--- website/data.php
+++ website/data.php
@@ -1,48 +1,58 @@
 <!-- Include file voor bootstrap, stylesheet etc. -->
-<?php include('header_menu.php'); ?>
+<?php
+include('header_menu.php');
+include('inc/functions.php');
+?>
 <?php
 if($_SESSION['rechten'] =='0') {
-		$_SESSION['errorlog']="U heeft geen rechten om deze pagina te bekijken";
+	$_SESSION['errorlog']="U heeft geen rechten om deze pagina te bekijken";
 	header('location: index.php');
 }?>
+<?php
+$csvFile = file('data/data.csv');
+    $data = [];
+    foreach ($csvFile as $line) {
+        $data[] = str_getcsv($line);
+    }
+
+		// $alles = $data.length;
+?>
+<!DOCTYPE html>
 <html>
-  <head>
-   <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-   <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-   <script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
+<head>
+</head>
+<body>
+	<?php if (count($data) > 0):
+		?>
+	<table class="table">
+		<thread>
+			<tr>
+				<th scope="col">Station Number</th>
+				<th scope="col">Date</th>
+				<th scope="col">Time</th>
+				<th scope="col">Temperature</th>
+				<th scope="col">Rainfall</th>
+				<th scope="col">Wind Direction</th>
+				<th scope="col">Humidity</th>
+			</tr>
+		</thread>
+		<tbody>
+			<?php
+			$length = count($data);
 
-          var queryOptions = {
-            csvColumns: ['number', 'number'],
-            csvHasHeader: true
-          }
-
-          var query = new google.visualization.Query('http://localhost/website/data/testdata.csv', queryOptions);
-					query.setQuery('select A, B group by A');
-          query.send(handleQueryResponse);
-     }
-
-     function handleQueryResponse (response) {
-
-			 if (response.isError()) {
-	    	alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-	    	return;
-        }
-
-        var data = response.getDataTable();
-				data.addColumn('number', 'TEMP');
-				data.addColumn('number', 'DATE');
-
-        var chart = new google.visualization.LineChart(document.getElementById('chart'));
-
-
-        chart.draw(data);
-      }
-    </script>
-  </head>
-  <body>
-<div id="chart" style="width: 100%; height: 100%;"></div>
+			for ($i = 1; $i <= $length; $i++) { ?>
+			<tr>
+				<th><?php echo $data[$i][0];?></th>
+				<td><?php echo $data[$i][1];?></td>
+				<td><?php echo $data[$i][2];?></td>
+				<td><?php echo $data[$i][5];?></td>
+				<td><?php echo $data[$i][9];?></td>
+				<td><?php echo $data[$i][13];?></td>
+				<td><?php echo $data[$i][3];?></td>
+			</tr>
+		<?php } ?>
+		</tbody>
+	</table>
+<?php endif; ?>
 </body>
 </html>
